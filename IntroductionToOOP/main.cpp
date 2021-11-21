4f8ac4d49b52474a9e903dcf6d8227ff96da40e3
mironenko_ev@mail.ru <mironenko_ev@mail.ru>
2021-11-22 00:23:39 +0500

    Add project files.

---
+++ IntroductionToOOP/main.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+#define tab "\t"
+class Point
+{
+	double x;
+	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+};
+//#define STRUCT_POINT
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+#ifdef STRUCT_POINT
+	int a;
+	Point A;
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << tab << A.y << endl;
+	Point* pA = &A;
+	cout << pA->x << tab << pA->y << endl;
+#endif // STRUCT_POINT
+
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << tab << A.get_y() << endl;
+
+}
