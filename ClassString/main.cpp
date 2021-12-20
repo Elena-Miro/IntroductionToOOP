c218627f6bd57444113122d25e5d4d535f03f647
mironenko_ev@mail.ru <mironenko_ev@mail.ru>
2021-12-21 00:07:09 +0500

    MetodsOutOfClass

--- ClassString/main.cpp
+++ ClassString/main.cpp
@@ -13,75 +13,90 @@ class String
 	char* str;//Адрес строки в динамической памяти
 
 public:
-	int get_size()const
-	{
-		return size;
-	}
-	const char* get_str()const
-	{
-		return str;
-	}
-	char* get_str()
-	{
-		return str;
-	}
-	explicit String(int size = 80) :size(size), str(new char[size] {})
-	{
-		//this->size = size;
-		//this->str = new char[size] {};//Память для строки обязательно зануляем
-		cout << "Constructor:\t" << this << endl;
-	}
-	String(const char str[]):String(strlen(str)+1)
-	{
-		//this->size = strlen(str) + 1;
-		//this->str = new char[size] {};
-		for (int i = 0; str[i]; i++)this->str[i] = str[i];
-		cout << "Constructor:\t" << this << endl;
-	}
-	String(const String& other):String(other.str)//Побитовое копирование Deep copy
-	{
-		//this->size = other.size;
-		//this->str = new char[size] {};
-		//for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyConstructor:\t" << this << endl;
-	}
-	~String()
-	{
-		delete[]str;
-		cout << "Destructor:\t" << this << endl;
-
-	}
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
+	explicit String(int size = 80);
+	String(const char str[]);
+	String(const String& other);
+	~String();
 	//Operators
-	String & operator=(const String& other)
-	{
-		if (this == &other)return *this;//не являются ли this и other одним и тем же объектом
-		delete[] this->str;
-		this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyAssignment:\t" << this << endl;
-		return *this;
-	}
-	char& operator[](int i)
-	{
-		return str[i];
-	}
-	const char& operator[](int i) const//если пишем в конце const то и в начале
-	{
-		return str[i];
-	}
-	String& operator+=(const String& other)
-	{
-		return *this = *this + other;
-	}
+	String& operator=(const String& other);
+	char& operator[](int i);
+	const char& operator[](int i) const;
+	String& operator+=(const String& other);
 	//Metods
 
-	void print()const
-	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
-	}
+	void print()const;
 };
+int String:: get_size()const   //Scope operator
+{
+	return size;
+}
+const char* String:: get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+String::String(int size) :size(size), str(new char[size] {})
+{
+	//this->size = size;
+	//this->str = new char[size] {};//Память для строки обязательно зануляем
+	cout << "Constructor:\t" << this << endl;
+}
+String::String(const char str[]) :String(strlen(str) + 1)
+{
+	//this->size = strlen(str) + 1;
+	//this->str = new char[size] {};
+	for (int i = 0; str[i]; i++)this->str[i] = str[i];
+	cout << "Constructor:\t" << this << endl;
+}
+String::String(const String& other) :String(other.str)//Побитовое копирование Deep copy
+{
+	//this->size = other.size;
+	//this->str = new char[size] {};
+	//for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	cout << "CopyConstructor:\t" << this << endl;
+}
+String::~String()
+{
+	delete[]str;
+	cout << "Destructor:\t" << this << endl;
+
+}
+//Operators
+String& String::operator=(const String& other)
+{
+	if (this == &other)return *this;//не являются ли this и other одним и тем же объектом
+	delete[] this->str;
+	this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	cout << "CopyAssignment:\t" << this << endl;
+	return *this;
+}
+char& String:: operator[](int i)
+{
+	return str[i];
+}
+const char& String::operator[](int i) const//если пишем в конце const то и в начале
+{
+	return str[i];
+}
+String& String:: operator+=(const String& other)
+{
+	return *this = *this + other;
+}
+//Metods
+
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+}
 String operator+(const String& left, const String& right)
 {
 	String result(left.get_size() + right.get_size()-1);//убираем 1 детерминрующий ноль
@@ -113,7 +128,7 @@ std::istream& operator>>(std::istream& is, String& obj)
 	//return is >> obj.get_str();
 	const int SIZE = 10240;
 	char buffer[SIZE] = {};
-	is >> buffer;
+	is >> buffer;//is=cin ввод с клавиатуры
 	obj = buffer;
 	return is;
 }
